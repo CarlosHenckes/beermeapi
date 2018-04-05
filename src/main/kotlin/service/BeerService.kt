3ebc864b143ae40149001a9ddf52aa114e7696d0
Carlos Henckes <henckes@uol.com.br>
2018-04-05 08:05:05 -0300

    model added

---
+++ src/main/kotlin/service/BeerService.kt
@@ -0,0 +1,17 @@
+package service
+
+import com.beerme.beerme.model.Beer
+import com.beerme.beerme.repository.BeerRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+@Service
+class BeerService {
+
+    @Autowired
+    lateinit var beerRepository: BeerRepository
+
+    fun buscarTodasCervejas() : List<Beer>{
+        return beerRepository.findAll()
+    }
+}
