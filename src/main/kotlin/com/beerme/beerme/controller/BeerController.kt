0f96924907efcb929bbe7eed5f49d058d1ce244e
Carlos Henckes <henckes@uol.com.br>
2018-04-05 08:09:12 -0300

    controller calling service

--- src/main/kotlin/com/beerme/beerme/controller/BeerController.kt
+++ src/main/kotlin/com/beerme/beerme/controller/BeerController.kt
@@ -1,16 +1,22 @@
 package com.beerme.beerme.controller
 
+import com.beerme.beerme.model.Beer
 import com.sun.xml.internal.fastinfoset.util.StringArray
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import service.BeerService
 
 @RestController
 @RequestMapping("/beer")
 class BeerController {
+
+    @Autowired
+    lateinit var beerService: BeerService
+
     @GetMapping
-    fun buscarTodos() : Array<String>{
-        val list = arrayOf("carlos","marcus","carla","ailton","jose")
-        return list
+    fun buscarTodos() : List<Beer>{
+        return beerService.buscarTodasCervejas()
     }
 }
