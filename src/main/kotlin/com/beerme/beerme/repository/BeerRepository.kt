480365726fcb00f77ad5bc1a8b7c708b565508bc
Carlos Henckes <henckes@uol.com.br>
2018-04-05 07:43:53 -0300

    model added

---
+++ src/main/kotlin/com/beerme/beerme/repository/BeerRepository.kt
@@ -0,0 +1,10 @@
+package com.beerme.beerme.repository
+
+import com.beerme.beerme.model.Beer
+import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface BeerRepository: MongoRepository<Beer, String> {
+    fun findByRotulo(rotulo: String): List<Beer>
+}
