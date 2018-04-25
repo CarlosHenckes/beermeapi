2a51f86281b618d1630580de2982c13dbaf3727d
Carlos Henckes <henckes@uol.com.br>
2018-04-25 18:43:28 -0300

    order by

--- src/main/kotlin/com/beerme/beerme/repository/BeerRepository.kt
+++ src/main/kotlin/com/beerme/beerme/repository/BeerRepository.kt
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository
 @Repository
 interface BeerRepository: MongoRepository<Beer, String> {
     fun findByRotuloContainingIgnoreCase(rotulo: String): List<Beer>?
+    
+    fun findAllByOrderByRotuloAsc(): List<Beer>?
 }
